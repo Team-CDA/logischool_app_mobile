ad2f0913c0cab71322bf4d9125ec9533d98b9b10
NADIR BEN SALAH <95688259+Nadir-Bensalah@users.noreply.github.com>
2023-05-25 14:48:08 +0200

    up

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from "native-base";
-import TabBarNavigation from './src/navigation/TabBarNavigation';
-
+import DrawerNavigation from './src/navigation/DrawerNavigation';
 
 
 export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-        <TabBarNavigation/>
+        <DrawerNavigation/>
       </NativeBaseProvider>
     </NavigationContainer>
   );
--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,18 +1,25 @@
- 
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import CustomHeader from '../components/CustomHeader'; 
+import ProfileScreen from '../screens/SettingScreen';
+import TroisiemeEcran from '../screens/TroisiemeEcran';
+import QuatriemeEcran from '../screens/QuatriemeEcran';
+import NotificationScreen from '../screens/NotificationScreen';
+import TabBarNavigation from './TabBarNavigation'; 
 
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigation = () => {
+function DrawerNavigation() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="CustomHeader" component={CustomHeader} />
+    <Drawer.Navigator 
+    initialRouteName="TabBarNavigation" 
+    screenOptions={{
+      headerShown: false 
+    }}>
+      <Drawer.Screen name="Accueil" component={TabBarNavigation} />
+      <Drawer.Screen name="Notification" component={NotificationScreen} />
+
     </Drawer.Navigator>
   );
 }
 
 export default DrawerNavigation;
-
- 
