06346e1edb9e3c474179123ff663339f9523dbb0
NADIR BEN SALAH <95688259+Nadir-Bensalah@users.noreply.github.com>
2023-06-27 11:45:24 +0200

    Calendrier Mobile fini à 80%

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,49 +1,65 @@
+// Importer les modules nécessaires
 import React, { useState, useEffect, useRef, useMemo } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, RefreshControl } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, RefreshControl, ScrollView } from 'react-native';
 import { Agenda } from 'react-native-calendars';
 import moment from 'moment';
 import 'moment/locale/fr';
 import { LocaleConfig } from 'react-native-calendars';
 import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import { getSchedules } from '../utils/axios';  
+import { getSchedules } from '../utils/axios'; 
 
+// Configuration de la langue pour la date et le calendrier  
 LocaleConfig.locales['fr'] = {
   monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
   monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juill.','Août','Sept.','Oct.','Nov.','Déc.'],
   dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
   dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.']
 };
-
 LocaleConfig.defaultLocale = 'fr';
 moment.locale('fr');
 
+// Couleurs prédéfinies pour les différentes matières 
+const subjectColors = {
+  Mathématiques: '#0000FF',
+  Anglais: '#FFA500',
+};
+
+// Une fonction asynchrone pour obtenir les éléments à partir de l'API 
 const fetchItems = async () => {
   try {
+    // Appeler l'API
     const apiItems = await getSchedules();
     const newItems = {};
 
+    // Parcourir les éléments et les formatter comme nécessaire 
     for (const item of apiItems) {
+      // Extraire la date, l'heure de début et de fin à partir de l'élément 
       const date = moment(item.lesson_date).format('YYYY-MM-DD');
       const start = moment(item.lesson_date).format('HH:mm');
       const end = moment(item.lesson_date).add(item.duration, 'minutes').format('HH:mm');
+      
+      // Déterminer la couleur de l'élément en fonction de la matière 
+      const color = subjectColors[item.subject_name] || '#ff6347';
 
+      // Ajouter l'élément à newItems 
       if (newItems[date]) {
         newItems[date].push({
           name: item.subject_name,
           start,
           end,
-          color: '#ff6347',  // Remplacez cette valeur fixe par une couleur dynamique si vous le souhaitez
+          color,
         });
       } else {
         newItems[date] = [{
           name: item.subject_name,
           start,
           end,
-          color: '#ff6347',  // Remplacez cette valeur fixe par une couleur dynamique si vous le souhaitez
+          color,
         }];
       }
     }
 
+    // Ajouter des notes pour les week-ends 
     addWeekendNotes(newItems);
     return newItems;
   } catch (error) {
@@ -51,12 +67,14 @@ const fetchItems = async () => {
   }
 };
 
+// Une fonction pour ajouter des notes pour les week-ends 
 const addWeekendNotes = (items) => {
   for (let i = -90; i < 90; i++) {
     const time = moment().add(i, 'days');
     const strTime = time.format('YYYY-MM-DD');
 
-    if (time.weekday() === 5 || time.weekday() === 6) { // Check if it's a weekend
+    // Si le jour est un week-end, ajouter une note
+    if (time.weekday() === 5 || time.weekday() === 6) {
       if (items[strTime]) {
         items[strTime].push({
           name: 'Pas de cours à cette date',
@@ -76,46 +94,54 @@ const addWeekendNotes = (items) => {
   }
 };
 
+
+// Le composant principal 
 const CalendarScreen = () => {
+  // Déclarer les hooks d'état pour les éléments, l'élément sélectionné, le jour sélectionné et l'indicateur de rafraîchissement 
   const [items, setItems] = useState({});
   const [selectedItem, setSelectedItem] = useState(null);
-  const [selectedDay, setSelectedDay] = useState(null); // Ajouter cette ligne
-  const [isRefreshing, setIsRefreshing] = useState(false); // Nouveau
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+  
+  // Référence à la feuille inférieure 
   const bottomSheetModalRef = useRef(null);
-
+  
+  // Points de capture pour le modal 
   const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
 
-  const fetchItemsAndSetState = async () => {
-    setIsRefreshing(true);
-    const fetchedItems = await fetchItems();
-    setItems(fetchedItems);
-    setIsRefreshing(false);
-  }
-
+  // Appeler la fonction fetchItems au montage du composant 
   useEffect(() => {
-    fetchItemsAndSetState();
+    fetchItems().then(setItems);
   }, []);
 
-  const onRefresh = () => {
-    fetchItemsAndSetState();
-  };
+  // Fonction pour rafraîchir les éléments 
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    fetchItems().then(newItems => {
+      setItems(newItems);
+      setRefreshing(false);
+    });
+  }, []);
 
+  // Fonction pour gérer le clic sur un jour 
   const onDayPress = (day) => {
-    setSelectedDay(day.dateString); // Modifier cette ligne
+    setSelectedDay(day.dateString);
   };
 
+  // Fonction pour ouvrir la feuille de bas de page 
   const openBottomSheetModal = (item) => {
     setSelectedItem(item);
     bottomSheetModalRef.current.present();
   };
 
+  // Fonction pour obtenir les dates marquées 
   const getMarkedDates = () => {
     let markedDates = {};
-  
+
     for (let i = -90; i < 90; i++) {
       const time = moment().add(i, 'days');
       const strTime = time.format('YYYY-MM-DD');
-  
+
       if (items[strTime]) {
         markedDates[strTime] = {
           selected: true,
@@ -125,10 +151,11 @@ const CalendarScreen = () => {
         };
       }
     }
-  
+
     return markedDates;
   };
 
+  // Fonction pour rendre les éléments 
   const renderItems = () => {
     if (!selectedDay) {
       return items;
@@ -137,30 +164,24 @@ const CalendarScreen = () => {
     return { [selectedDay]: items[selectedDay] };
   }
 
+  // Le rendu du composant 
   return (
     <BottomSheetModalProvider>
-      <View style={styles.container}>
-        <ScrollView
-          contentContainerStyle={{ flex: 1 }}
-          refreshControl={
-            <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
-          }
-        >
-          <Agenda
-            firstDay={1}
-            items={renderItems()} // Modifier cette ligne
-            onDayPress={onDayPress}
-            markedDates={getMarkedDates()}
-            renderItem={(item, firstItemInDay) => (
-              <TouchableOpacity onPress={() => openBottomSheetModal(item)}>
-                <View style={[styles.item, {backgroundColor: item.name === 'Pas de cours à cette date' ? 'gray' : item.color}]}>
-                  <Text style={styles.itemTextTitle}>{item.name}</Text>
-                  <Text style={styles.itemText}>{item.start} - {item.end}</Text>
-                </View>
-              </TouchableOpacity>
-            )}
-          />
-        </ScrollView>
+      <ScrollView contentContainerStyle={styles.container} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        <Agenda
+          firstDay={1}
+          items={renderItems()}
+          onDayPress={onDayPress}
+          markedDates={getMarkedDates()}
+          renderItem={(item, firstItemInDay) => (
+            <TouchableOpacity onPress={() => openBottomSheetModal(item)}>
+              <View style={[styles.item, {backgroundColor: item.color}]}>
+                <Text style={styles.itemTextTitle}>{item.name}</Text>
+                <Text style={styles.itemText}>{item.start} - {item.end}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
         <BottomSheetModal
           ref={bottomSheetModalRef}
           snapPoints={snapPoints}
@@ -171,14 +192,15 @@ const CalendarScreen = () => {
             <View style={styles.contentContainer}>
               <Text style={styles.modalText}>{selectedItem.name}</Text>
               <Text style={styles.modalText}>{selectedItem.start} - {selectedItem.end}</Text>
-             </View>
+            </View>
           )}
         </BottomSheetModal>
-      </View>
+      </ScrollView>
     </BottomSheetModalProvider>
   );
 };
 
+// Styles pour le composant 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -216,4 +238,5 @@ const styles = StyleSheet.create({
   },
 });
 
+// Exporter le composant 
 export default CalendarScreen;
