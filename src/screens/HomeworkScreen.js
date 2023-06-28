93e15c83930e6bbbba408050f687015b9a4d2446
Maskwelldev <102791393+Maskwelldev@users.noreply.github.com>
2023-06-29 01:36:40 +0200

    in progress : homeworks and fileviewer

--- src/screens/HomeworkScreen.js
+++ src/screens/HomeworkScreen.js
@@ -4,14 +4,48 @@ import {Picker} from '@react-native-picker/picker';
 import axiosInstance from '../utils/interceptor';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {format} from 'date-fns';
+import RNFS from 'react-native-fs';
+import FileViewer from 'react-native-file-viewer';
 
-const HomeworkScreen = () => {
+const handleDownloadFile = async (fileName) => {
+    const downloadDest = `${RNFS.DocumentDirectoryPath}/${fileName}`;
+    const fileUrl = `http://10.0.2.2:3000/files/${fileName}`;
+    
+        try {
+        const response = await RNFS.downloadFile({
+            fromUrl: fileUrl,
+            toFile: downloadDest,
+        }).promise;
+    
+        if (response.statusCode === 200) {
+            console.log('Fichier téléchargé :', downloadDest);
+    
+            const fileExists = await RNFS.exists(downloadDest);
+            console.log('Le fichier existe :', fileExists);
+            
+            FileViewer.open(downloadDest)
+            .then(() => {
+                console.log('Fichier ouvert avec succès');
+            })
+            .catch(error => {
+                console.log('Erreur lors de l\'ouverture du fichier :', error);
+            });
+        } else {
+            console.log('Erreur lors du téléchargement du fichier :', response);
+        }
+        } catch (error) {
+        console.log('Erreur globale lors du téléchargement du fichier :', error);
+        }
+    };
+    
+    const HomeworkScreen = () => {
     const [data, setData] = useState([]);
     const [subjects, setSubjects] = useState([]);
     const [userClass, setUserClass] = useState(null);
     const [subjectId, setSubjectId] = useState(null);
     const [userId, setUserId] = useState(null);
-    const [displayType, setDisplayType] = useState('all');
+    const [displayType, setDisplayType] = useState('courses');
 
     useEffect(() => {
         const fetchUserInfo = async () => {
@@ -36,16 +70,16 @@ const HomeworkScreen = () => {
 
     useEffect(() => {
         axiosInstance
-            .get('/subjects')
-            .then(response => {
+        .get('/subjects')
+        .then(response => {
             if (response.data) {
-                console.log('Subjects', response.data);
-                setSubjects(response.data);
+            console.log('Subjects', response.data);
+            setSubjects(response.data);
             }
-            })
-            .catch(error => {
+        })
+        .catch(error => {
             console.error(error);
-            });
+        });
     }, []);
 
     useEffect(() => {
@@ -65,83 +99,132 @@ const HomeworkScreen = () => {
         }
     }, [userClass, subjectId]);
 
-    const renderItem = ({item}) => (
-        <View style={styles.item}>
+    const renderItem = ({ item }) => {
+        const now = new Date();
+    
+        if (displayType === 'courses' && item.course_image) {
+        return (
+            <View style={styles.item}>
             <Text style={styles.title}>{item.name}</Text>
-            {(displayType === 'all' || displayType === 'courses') && item.course_image ? (
-                <View style={styles.course}>
-                    <Text style={styles.courseTitle}>Cours</Text>
+            <View style={styles.course}>
+                <Text style={styles.courseTitle}>Cours</Text>
+                <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    handleDownloadFile(item.course_image);
+                }}
+                >
+                <Icon name="download" size={16} color="#fff" style={styles.buttonIcon} />
+                <Text style={styles.buttonText}>Télécharger</Text>
+                </TouchableOpacity>
+            </View>
+            </View>
+        );
+        }
+    
+        if (displayType === 'homeworks') {
+        const isCorrectionDatePassed =
+            item.date_correction &&
+            format(new Date(item.date_correction), 'yyyy-MM-dd') <
+            format(now, 'yyyy-MM-dd');
+        const isHomeworkDateFuture =
+            item.date_devoir &&
+            format(new Date(item.date_devoir), 'yyyy-MM-dd') >
+            format(now, 'yyyy-MM-dd');
+    
+        if (
+            item.homework_image ||
+            (isCorrectionDatePassed && item.correction_image)
+        ) {
+            return (
+            <View style={styles.item}>
+                <Text style={styles.title}>{item.name}</Text>
+                {item.homework_image && (
+                <View style={styles.homework}>
+                    <Text style={styles.homeworkTitle}>Devoir</Text>
                     <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => {
-                            // TODO: Handle file download here
-                        }}
+                    style={styles.button}
+                    onPress={() => {
+                        handleDownloadFile(item.homework_image);
+                    }}
                     >
-                        <Icon name="download" size={16} color="#fff" style={styles.buttonIcon} />
-                        <Text style={styles.buttonText}>Télécharger le cours</Text>
+                    <Icon name="download" size={16} color="#fff" style={styles.buttonIcon} />
+                    <Text style={styles.buttonText}>Télécharger</Text>
                     </TouchableOpacity>
                 </View>
-            ) : null}
-            {(displayType === 'all' || displayType === 'homeworks') && (item.homework_image || item.correction_image) ? (
-                <>
-                    {item.homework_image && (
-                        <View style={styles.homework}>
-                            <Text style={styles.homeworkTitle}>Devoirs</Text>
-                            <TouchableOpacity
-                                style={styles.button}
-                                onPress={() => {
-                                    // TODO: Handle file download here
-                                }}
-                            >
-                                <Icon name="download" size={16} color="#fff" style={styles.buttonIcon} />
-                                <Text style={styles.buttonText}>Télécharger le devoir</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                    {item.correction_image && (
-                        <View style={styles.correction}>
-                            <Text style={styles.correctionTitle}>Corrections</Text>
-                            <TouchableOpacity
-                                style={styles.button}
-                                onPress={() => {
-                                    // TODO: Handle file download here
-                                }}
-                            >
-                                <Icon name="download" size={16} color="#fff" style={styles.buttonIcon} />
-                                <Text style={styles.buttonText}>Télécharger la correction</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                </>
-            ) : null}
-        </View>
-    );
+                )}
+                {isCorrectionDatePassed && item.correction_image && (
+                <View style={styles.correction}>
+                    <Text style={styles.correctionTitle}>Correction</Text>
+                    <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        handleDownloadFile(item.correction_image);
+                    }}
+                    >
+                    <Icon name="download" size={16} color="#fff" style={styles.buttonIcon} />
+                    <Text style={styles.buttonText}>Télécharger</Text>
+                    </TouchableOpacity>
+                </View>
+                )}
+                {isHomeworkDateFuture && (
+                <Text style={styles.deadlineText}>
+                    Date limite : {format(new Date(item.date_devoir), 'dd/MM/yyyy')}
+                </Text>
+                )}
+            </View>
+            );
+        }
+        }
     
-    
-    
-    
-    
-
-    const handleSubjectChange = (value) => {
-        setSubjectId(value);
+        return null;
     };
-
-    const handleDisplayTypeChange = (value) => {
-        setDisplayType(value);
+    
+    const handleSubjectChange = value => {
+        setSubjectId(value);
     };
 
     return (
         <View style={styles.container}>
         <Picker selectedValue={subjectId} onValueChange={handleSubjectChange}>
             {subjects.map(subject => (
-            <Picker.Item key={subject.id} label={subject.subject_name} value={subject.id} />
+            <Picker.Item
+                key={subject.id}
+                label={subject.subject_name}
+                value={subject.id}
+            />
             ))}
         </Picker>
-        <Picker selectedValue={displayType} onValueChange={handleDisplayTypeChange}>
-            <Picker.Item key="all" label="Tout afficher" value="all" />
-            <Picker.Item key="courses" label="Cours uniquement" value="courses" />
-            <Picker.Item key="homeworks" label="Devoirs et corrections" value="homeworks" />
-        </Picker>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+            style={[
+                styles.button,
+                displayType === 'courses' ? styles.activeButton : null,
+            ]}
+            onPress={() => setDisplayType('courses')}>
+            <Text
+                style={[
+                styles.buttonText,
+                displayType === 'courses' ? styles.activeButtonText : null,
+                ]}>
+                Cours
+            </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={[
+                styles.button,
+                displayType === 'homeworks' ? styles.activeButton : null,
+            ]}
+            onPress={() => setDisplayType('homeworks')}>
+            <Text
+                style={[
+                styles.buttonText,
+                displayType === 'homeworks' ? styles.activeButtonText : null,
+                ]}>
+                Devoirs et Corrections
+            </Text>
+            </TouchableOpacity>
+        </View>
         <FlatList
             data={data}
             renderItem={renderItem}
@@ -149,9 +232,9 @@ const HomeworkScreen = () => {
         />
         </View>
     );
-};
+    };
 
-const styles = StyleSheet.create({
+    const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
@@ -163,7 +246,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 16,
         borderRadius: 8,
         shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
+        shadowOffset: {width: 0, height: 2},
         shadowOpacity: 0.3,
         shadowRadius: 4,
         elevation: 2,
@@ -196,6 +279,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     button: {
+        flex: 1,
         backgroundColor: '#03395E',
         paddingHorizontal: 10,
         paddingVertical: 5,
@@ -205,16 +289,30 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    activeButton: {
+        backgroundColor: '#FFF',
+        borderColor: '#03395E',
+        borderWidth: 1,
+    },
     buttonText: {
+        paddingHorizontal: 8,
         fontSize: 16,
         color: '#fff',
     },
-    buttonIcon: {
-        marginRight: 10,
+    activeButtonText: {
+        // fontWeight: 'bold',
+        color: '#03395E',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 10,
+    },
+    deadlineText: {
+        fontSize: 16,
+        color: '#022D47',
+        marginTop: 10,
     },
 });
 
-
-
 export default HomeworkScreen;
-
